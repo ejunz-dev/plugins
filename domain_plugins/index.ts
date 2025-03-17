786636184ff5ff10197acf9cd3307f660fe4978a
Jacka10086 <496806255@qq.com>
2025-03-17 20:13:53 +0800

    init manage_plugins

--- domain_plugins/index.ts
+++ domain_plugins/index.ts
@@ -22,6 +22,34 @@ class ManageHandler extends Handler {
         this.domain = await DomainModel.get(domainId);
     }
 }
+class SystemPluginHandler extends ManageHandler {
+    @requireSudo
+    async get({ domainId }) {
+        this.response.template = 'manage_plugins.html';
+        this.response.body = {
+            domain: this.domain,
+            plugins: loadedPlugins,
+        };
+        console.log('this.response.body', this.response.body);
+    }
+
+    // @requireSudo
+    // async post({ domainId }) {
+    //     const roles = {};
+    //     for (const role in this.request.body) {
+    //         const perms = this.request.body[role] instanceof Array
+    //             ? this.request.body[role]
+    //             : [this.request.body[role]];
+    //         roles[role] = 0n;
+    //         for (const r of perms) roles[role] |= 1n << BigInt(r);
+    //     }
+    //     await Promise.all([
+    //         DomainModel.setRoles(domainId, roles),
+    //     ]);
+    //     this.back();
+    // }
+}
+
 class DomainPluginHandler extends ManageHandler {
     @requireSudo
     async get({ domainId }) {
@@ -49,15 +77,16 @@ class DomainPluginHandler extends ManageHandler {
     //     this.back();
     // }
 }
-
 export async function apply(ctx: Context) {
     global.Ejunz.ui.inject('DomainManage', 'domain_plugins', { family: 'Access Control', icon: 'user' });
-    console.log('Ejunz.ui.INJECT', global.Ejunz.ui.inject);
+    global.Ejunz.ui.inject('NavDropdown', 'manage_plugins', { prefix: 'manage' }, PRIV.PRIV_EDIT_SYSTEM);
 
     ctx.Route('domain_plugins', '/domain/plugins', DomainPluginHandler);
-    
+    ctx.Route('manage_plugins', '/manage/plugins', SystemPluginHandler);
+
     ctx.i18n.load('zh', {
         domain_plugins: '管理插件',
+        manage_plugins: '系统插件',
     });
 
     ctx.injectUI('Home_Domain', 'domain_plugins', (h) => ({
