458c536204b06dad32fe6511b1c1fcf8146fb4c7
Jacka10086 <496806255@qq.com>
2025-03-21 11:20:06 +0800

    add PLUGINS_PERMS

--- domain_plugins/index.ts
+++ domain_plugins/index.ts
@@ -6,12 +6,11 @@ import {
     InvalidJoinInvitationCodeError, OnlyOwnerCanDeleteDomainError, PermissionError, RoleAlreadyExistError, ValidationError,
 } from 'ejun';
 import type { DomainDoc } from 'ejun';
-import { PERM, PERMS_BY_FAMILY, PRIV } from 'ejun';
+import { PERM, PERMS_BY_FAMILY, PRIV, PLUGINS_PERMS_BY_FAMILY } from 'ejun';
 import * as discussion from 'ejun';
-import * as oplog from 'ejun';
 import * as system from 'ejun';
 import {
-    Handler, param, post, query, requireSudo, Types,domain,Context,DomainModel
+    Handler, param, post, query, requireSudo, Types,domain,Context,DomainModel,OplogModel
 } from 'ejun';
 import { log2 } from 'ejun'
 import { loadedPlugins } from 'ejun';
@@ -53,30 +52,33 @@ class SystemPluginHandler extends ManageHandler {
 class DomainPluginHandler extends ManageHandler {
     @requireSudo
     async get({ domainId }) {
+        const roles = await DomainModel.getRoles(domainId);
         this.response.template = 'domain_plugins.html';
         this.response.body = {
-            domain: this.domain,
-            plugins: loadedPlugins,
+            roles, PLUGINS_PERMS_BY_FAMILY, domain: this.domain, log2,
         };
         console.log('this.response.body', this.response.body);
     }
 
-    // @requireSudo
-    // async post({ domainId }) {
-    //     const roles = {};
-    //     for (const role in this.request.body) {
-    //         const perms = this.request.body[role] instanceof Array
-    //             ? this.request.body[role]
-    //             : [this.request.body[role]];
-    //         roles[role] = 0n;
-    //         for (const r of perms) roles[role] |= 1n << BigInt(r);
-    //     }
-    //     await Promise.all([
-    //         DomainModel.setRoles(domainId, roles),
-    //     ]);
-    //     this.back();
-    // }
+    @requireSudo
+    async post({ domainId }) {
+        const roles = {};
+        for (const role in this.request.body) {
+            const perms = this.request.body[role] instanceof Array
+                ? this.request.body[role]
+                : [this.request.body[role]];
+            roles[role] = 0n;
+            for (const r of perms) roles[role] |= 1n << BigInt(r);
+        }
+        await Promise.all([
+            DomainModel.setRoles(domainId, roles),
+            OplogModel.log(this, 'domain.setRoles', { roles }),
+        ]);
+        this.back();
+    }
 }
+
+
 export async function apply(ctx: Context) {
     global.Ejunz.ui.inject('DomainManage', 'domain_plugins', { family: 'Access Control', icon: 'user' });
     global.Ejunz.ui.inject('NavDropdown', 'manage_plugins', { prefix: 'manage' }, PRIV.PRIV_EDIT_SYSTEM);
@@ -85,7 +87,7 @@ export async function apply(ctx: Context) {
     ctx.Route('manage_plugins', '/manage/plugins', SystemPluginHandler);
 
     ctx.i18n.load('zh', {
-        domain_plugins: '插件',
+        domain_plugins: '管理插件',
         manage_plugins: '系统插件',
     });
 
