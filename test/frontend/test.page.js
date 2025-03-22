7e5b97bd8646852098a1edae0899d7eb0d4738db
Jacka10086 <496806255@qq.com>
2025-03-23 07:22:06 +0800

    mvp cater/logo

--- test/frontend/test.page.js
+++ test/frontend/test.page.js
@@ -14,7 +14,12 @@ addPage(new AutoloadPage('testPage', async () => {
   const $logo = getLogoDom();
   if (target === 'A001') {
     if ($logo) {
-      $logo.attr('src', 'https://picsum.photos/300/200');
+      $logo.attr('src', '/file/2/Counter-Strike_2_%2528Icon%2529.webp');
     }
-  } 
+  }
+  if (target === 'A002') {
+    if ($logo) {
+      $logo.attr('src', '/file/2/Yin_and_Yang_symbol.svg.png');
+    }
+  }
 }));
