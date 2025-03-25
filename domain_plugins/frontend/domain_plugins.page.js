7d78b9293915e3dbfc45d66616155c2beabbb122
Jacka10086 <496806255@qq.com>
2025-03-25 13:29:48 +0800

    add ui/domain_perm no-display

--- domain_plugins/frontend/domain_plugins.page.js
+++ domain_plugins/frontend/domain_plugins.page.js
@@ -4,6 +4,12 @@ import {
   } from '@ejunz/ui-default';
 
 addPage(new AutoloadPage('DomainPluginsPage', async () => {
+    $('.domain-users tbody').each(function() {
+        var familyName = $(this).find('tr:first-child td.col--family').text().trim();
+        if (familyName == 'plugins') {
+            $(this).css('display', 'none');
+        }
+    });
 
 $(document).ready(function() {
     $('.domain-user tbody').each(function() {
